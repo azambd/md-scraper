a4a547b78c7cdd550d8bd9d9b46d57674b501090
Faruque A <engrfazam@gmail.com>
2020-02-19 09:17:46 +0600

    set pandas to get results in csv

--- mdScraper/mdScraper/spiders/md_review.py
+++ mdScraper/mdScraper/spiders/md_review.py
@@ -3,6 +3,8 @@ import scrapy
 
 from selenium import webdriver
 
+import pandas as cv
+
 
 class MdReviewSpider(scrapy.Spider):
     name = 'md_review'
@@ -10,8 +12,20 @@ class MdReviewSpider(scrapy.Spider):
     start_urls = ['https://www.md.com/doctor/vk-puppala-1-md']
 
     def parse(self, response):
+        # set this w.r.t your environment
         self.driver = webdriver.Chrome('/home/azam/Documents/chromedriver')
+
         self.driver.get(response.url)
+
+        name_list = []
+        address_list = []
+        profile_pics_list = []
+        address_list = []
+        review_numbers_list = []
+        review_date_list = []
+        reviewer_name_list = []
+        review_list = []
+        hash_key_list = []
 
         name = self.driver.find_element_by_css_selector(
             'span[itemprop="name"]').text
@@ -20,7 +34,7 @@ class MdReviewSpider(scrapy.Spider):
         if address != None:
             address = address.replace('map', '').strip()
 
-        profile_pics = driver.find_element_by_css_selector(
+        profile_pics = self.driver.find_element_by_css_selector(
             'img[itemprop="photo"]').get_attribute('src')
 
         review_numbers = self.driver.find_element_by_css_selector('h4').text
@@ -36,8 +50,22 @@ class MdReviewSpider(scrapy.Spider):
             review = div.find_element_by_css_selector(
                 'div[itemprop="reviewBody"]').text
             hash_key = div.get_attribute('id')
-            print('\n\n', name, '\n\n', address, '\n\n', review_date, '\n\n',
-                  review_numbers, '\n\n', review, '\n\n\n', response.url, '\n\n')
+            print('\n\nName:', name, '\n\nAddress:', address, '\n\nReview Date:', review_date, '\n\nTotal Reviews:',
+                  review_numbers, '\n\nReview:', review, '\n\nProfile Link:', response.url, '\n\n')
             print('\n\n')
 
+            name_list.append(name)
+            address_list.append(address)
+            profile_pics_list.append(profile_pics)
+            address_list.append(address)
+            review_numbers_list.append(review_numbers)
+            review_date_list.append(review_date)
+            reviewer_name_list.append(reviewer_name)
+            review_list.append(review)
+            hash_key_list.append(hash_key)
+
+        df = cv.DataFrame(list(zip(name_list, address_list, profile_pics_list, address_list, review_numbers_list, review_date_list, reviewer_name_list, review_list, hash_key_list)), columns=[
+                          'name', 'address', 'profile_pics', 'address', 'review_numbers', 'review_date', 'reviewer_name', 'review', 'hash_key'])
+
+        df.to_csv('md_test_result.csv', index=False)
         self.driver.quit()
